929115434b5318c1ec86e8a013b206df780dc161
Long Hoang <long@mindworker.de>
2018-02-24 00:39:11 +0800

    Tests multiple token investors, their balances and whether goal is reached

--- test/testAvoCrowdsale.js
+++ test/testAvoCrowdsale.js
@@ -10,7 +10,7 @@ contract('AvoCrowdsale', function(accounts) {
     let accs = accounts;
     let owner = accs[0];
     let investor1 = accs[1];
-    let investor2 = accs[1];
+    let investor2 = accs[2];
 
     let d = new Date();
     let start = Math.round((new Date()).valueOf()/1000);
@@ -18,6 +18,8 @@ contract('AvoCrowdsale', function(accounts) {
     let rate = 100;
 
     let crowdsale;
+    let token;
+    
     const addressZero = "0x0000000000000000000000000000000000000000";
     const deposit0 = Math.floor(Math.random() * 1000) + 1;
 
@@ -33,25 +35,54 @@ contract('AvoCrowdsale', function(accounts) {
 	    web3.toWei(125000, "ether"),   // crowdsale cap
 	    owner,                         // fund collection wallet $$$
 	    { from: owner }
-	).then(instance => crowdsale = instance);
+	).then(async instance => {
+	    crowdsale = instance;
+	    token = AvoToken.at(await crowdsale.token.call());
+	});
     });
 
     describe("standard interaction", function() {
-	it("Avotoken test", function() {
+	it("Avotoken should create", function() {
 	    return AvoToken.new();
 	});
 	
-        it("Investor1 buys tokens", async function() {
-	    await AvoToken.new();
-	    
+        it("should process token purchase of Investor1", async function() {	    
 	    await crowdsale.buyTokens(
 		investor1,
-		{from: investor1, value: web3.toWei(1.23, "ether")}
+		{from: investor1, value: web3.toWei(0.23, "ether")}
 	    );
 
 	    let raised = await crowdsale.weiRaised.call();
-	    assert.strictEqual(raised.toString(), web3.toWei(1.23, "ether").toString());
+	    let balance1 = await token.balanceOf.call(investor1);
+	    let goalReached = await crowdsale.goalReached.call();
+	    
+	    assert.strictEqual(raised.toString(), web3.toWei(0.23, "ether").toString());
+	    assert.strictEqual(balance1.toString(), web3.toWei(0.23*rate, "ether").toString());
+	    assert.equal(goalReached, false);
 	});
+	
+        it("should process token purchase of Investor1 and Investor2", async function() {	    
+	    await crowdsale.buyTokens(
+		investor1,
+		{from: investor1, value: web3.toWei(0.23, "ether")}
+	    );
+	    await crowdsale.buyTokens(
+		investor2,
+		{from: investor2, value: web3.toWei(1.23, "ether")}
+	    );
+
+	    let raised = await crowdsale.weiRaised.call();
+	    let balance1 = await token.balanceOf.call(investor1);
+	    let balance2 = await token.balanceOf.call(investor2);
+	    let goalReached = await crowdsale.goalReached.call();
+	    
+	    assert.strictEqual(raised.toString(), web3.toWei(1.46, "ether").toString());
+	    assert.strictEqual(balance1.toString(), web3.toWei(0.23*rate, "ether").toString());
+	    assert.strictEqual(balance2.toString(), web3.toWei(1.23*rate, "ether").toString());
+	    assert.equal(goalReached, true);
+	});
+
+	
     });
 
 });
